32d1cae82ff00837c3301a647ca186a5f7e64cf3
Rybalov Gleb <rybalov.gleb@gmail.com>
2021-12-10 00:32:29 +0300

    Added main page + modified .gitignore

--- first/views.py
+++ first/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from datetime import date
+
+def index(request):
+    contex = {}
+    contex['date'] = date.today().strftime("%d/%m/%Y")
+    return render(request, 'index.html', contex)
