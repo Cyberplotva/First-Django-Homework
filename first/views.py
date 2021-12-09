c51e892a4af8b6022e329097c9a1d8feae456365
Rybalov Gleb <rybalov.gleb@gmail.com>
2021-12-10 00:58:59 +0300

    Added riddle and answer pages

--- first/views.py
+++ first/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def riddle(request):
+    return render(request, 'riddle.html')
+
+
+def answer(request):
+    return render(request, 'answer.html')
